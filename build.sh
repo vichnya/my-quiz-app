c852f6d1deb5597e0793d6d83e5ce1d43e9bb7a4
visnya_chern <73075390+vichnya@users.noreply.github.com>
2024-01-08 18:06:20 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 echo "Stop container"
 docker stop glav
 docker rm glav
-docker image rm vishnya1chern/my-nginx #${{ secrets.DOCKER_USERNAME }}/${{ secrets.DOCKER_REPO_NAME }}
+docker image rm vishnya1chern/my-nginx #DOCKER_USERNAME/DOCKER_REPO_NAME
 echo "Pull image"
-docker pull vishnya1chern/my-nginx #${{ secrets.DOCKER_USERNAME }}/${{ secrets.DOCKER_REPO_NAME }}
+docker pull vishnya1chern/my-nginx #DOCKER_USERNAME/DOCKER_REPO_NAME
 echo "Start frontend container"
-docker run -p 80:80 --name glav -d vishnya1chern/my-nginx #${{ secrets.DOCKER_USERNAME }}/${{ secrets.DOCKER_REPO_NAME }}
+docker run -p 80:80 --name glav -d vishnya1chern/my-nginx #DOCKER_USERNAME/DOCKER_REPO_NAME
 echo "Finish deploying!"
